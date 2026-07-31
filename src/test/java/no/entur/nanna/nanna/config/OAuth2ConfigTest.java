3e52c5dacbe51541673f9834e13dcc4980efbfec
erlendev <erlend.vige@gmail.com>
2026-07-31 13:52:29 +0200

    Improves OAuth2Config coverage

--- src/test/java/no/entur/nanna/nanna/config/OAuth2ConfigTest.java
+++ src/test/java/no/entur/nanna/nanna/config/OAuth2ConfigTest.java
@@ -3,12 +3,15 @@ package no.entur.nanna.nanna.config;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import org.entur.oauth2.multiissuer.MultiIssuerAuthenticationManagerResolver;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.util.ReflectionTestUtils;
 
 class OAuth2ConfigTest {
 
   private final OAuth2Config config = new OAuth2Config();
+
+  // --- parseAudiences ---
 
   @Test
   void parseAudiences_null_returnsEmptyList() {
@@ -33,6 +36,45 @@ class OAuth2ConfigTest {
   @Test
   void parseAudiences_commaSeparated_returnsAllAudiences() {
     assertThat(invoke("aud-a,aud-b")).containsExactly("aud-a", "aud-b");
+  }
+
+  @Test
+  void parseAudiences_threeAudiences_returnsAll() {
+    assertThat(invoke("aud-a,aud-b,aud-c"))
+      .containsExactly("aud-a", "aud-b", "aud-c");
+  }
+
+  // --- multiIssuerAuthenticationManagerResolver ---
+
+  @Test
+  void multiIssuerAuthenticationManagerResolver_isCreated_withNoProperties() {
+    MultiIssuerAuthenticationManagerResolver resolver =
+      config.multiIssuerAuthenticationManagerResolver("", "", "", "");
+    assertThat(resolver).isNotNull();
+  }
+
+  @Test
+  void multiIssuerAuthenticationManagerResolver_isCreated_withMultipleInternalAudiences() {
+    MultiIssuerAuthenticationManagerResolver resolver =
+      config.multiIssuerAuthenticationManagerResolver(
+        "aud-a,aud-b",
+        "",
+        "",
+        ""
+      );
+    assertThat(resolver).isNotNull();
+  }
+
+  @Test
+  void multiIssuerAuthenticationManagerResolver_isCreated_withMultiplePartnerAudiences() {
+    MultiIssuerAuthenticationManagerResolver resolver =
+      config.multiIssuerAuthenticationManagerResolver(
+        "",
+        "",
+        "aud-c,aud-d",
+        ""
+      );
+    assertThat(resolver).isNotNull();
   }
 
   @SuppressWarnings("unchecked")
